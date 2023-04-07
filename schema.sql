c6d4629fbf738b0fe40d2cfda04796612e3c681d
Habtamu210 <beleyaha@gmail.com>
2023-04-07 05:42:37 -0700

    Add animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+id integer primary key generated always as identity,
+name VARCHAR(30) NOT NULL,
+date_of_birth DATE DEFAULT CURRENT_DATE,
+escape_attempts INT DEFAULT 0,
+neutered BOOLEAN,
+weight_kg DECIMAL
 );
