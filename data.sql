53f8f18dc05e3607dd7535f5a51468458180b805
Habtamu210 <beleyaha@gmail.com>
2023-04-08 00:47:26 -0700

    Add relationships

--- data.sql
+++ data.sql
@@ -1,26 +1,61 @@
 /* Populate database with sample data. */
-INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-    VALUES (1, 'Agumon', '2020-03-02', 0, 'true', 10.23);
-INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-    VALUES (2, 'Gabumon', '2018-11-15', 2, 'true', 8);
-INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-    VALUES (3, 'Pikachu', '2021-01-07', 1, 'false', 15.04);
-INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-    VALUES (4, 'Devimon', '2017-05-12', 5, 'true', 11);
+/* Populate database with sample data. */
+/*day-1*/
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES 
+('Agumon', DATE '2020-02-03', 0, true, 10.23),
+('Gabumon', DATE '2018-11-15', 2, true, 8),
+('Pikachu', DATE '2021-01-07', 1, false, 15.04),
+('Devimon', DATE '2017-05-12', 5, true, 11);
 
-/* DAY TWO */
+/*day-2*/
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg)
+VALUES 
+('Charmander', DATE '2020-02-08', 0, false, -11),
+('Plantmon', DATE '2021-11-15', 2, true, -5.7),
+('Squirtle', DATE '1993-05-02', 3, false, -12.13),
+('Angemon', DATE '2005-06-12', 1, true, -45),
+('Boarmon', DATE '2005-06-07', 7, true, 20.4),
+('Blossom', DATE '1998-10-13', 3, true, 17),
+('Ditto', DATE '2022-05-14', 4, true, 22);
 
-INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (5, 'Charmander', '2020-02-08', 0, 'false', -11);
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (6,'Plantmon', '2021-11-15', 2, 'false', -5.7)
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (7, 'Squirtle', '1993-04-02', 3, 'false', -12.13)
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (8, 'Angemon', '2005-06-12', 1, 'true', -45)
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (9, 'Boarmon', '2005-06-07', 7, 'true', 20.4)
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (10, 'Blossom', '1998-10-13', 3, 'true', 17)
-	INSERT into animals (id, name, date_of_birth, escaped_attempts, neutered, weight_kg)
-	  VALUES (11, 'Ditto', '2022-05-14', 4, 'true', 22)
+/*DAY-3*/
+
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+SELECT * FROM species;
+
+--SET SPECIES_ID
+BEGIN;
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+COMMIT;
+SELECT * FROM animals;
+
+--SET OWNER_ID
+BEGIN;
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith' ) WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
+COMMIT;
+SELECT * FROM animals;
--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
-/* Database schema to keep the structure of entire database. */
+/* schema for animals table. */
+
+DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals (
 id integer primary key generated always as identity,
 name VARCHAR(30) NOT NULL,
@@ -10,4 +13,24 @@ weight_kg DECIMAL
 
 /*day-2*/
 ALTER TABLE animals
-ADD species VARCHAR(255);
+ADD species VARCHAR(80);
+
+/*day-3*/
+CREATE TABLE owners (
+    id INT primary key generated always as identity,
+    full_name VARCHAR(80) NOT NULL,
+    age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id INT primary key generated always as identity,
+    name VARCHAR(80) NOT NULL
+);
+
+BEGIN;
+ALTER TABLE animals 
+    DROP COLUMN species,
+    ADD species_id INT NULL REFERENCES species(id) ON DELETE CASCADE,
+    ADD owner_id INT NULL REFERENCES owners(id) ON DELETE CASCADE;
+COMMIT;
+SELECT * FROM animals;
